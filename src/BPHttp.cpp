ffdf4196dd9e692cf0756b438bf9fea3cb762de6
logan <loganmitchell2011@gmail.com>
2023-03-27 21:53:52 -0400

    fixed regexs to meet http requirements.

--- src/BPHttp.cpp
+++ src/BPHttp.cpp
@@ -15,25 +15,23 @@ BPHttpMessage::~BPHttpMessage() {
 
 void BPHttpMessage::Parse(std::vector<std::string> lines) {
     std::smatch match;
-    std::string pattern_req = "(POST|GET|PUT|HEAD|DELETE|CONNECT|OPTIONS|TRACE)\\s+([a-zA-Z\\/.-]+)\\s+([a-zA-Z]+)\\/([0-9.]+)$";
-    std::string pattern_resp = "HTTP\\/([0-9.]+)\\s+([0-9]+)\\s+(.*)";
-    std::string pattern_header = "^([a-zA-Z-]+):\\s+(.*)\n*";
+    std::string pattern_req = "(POST|GET|PUT|HEAD|DELETE|CONNECT|OPTIONS|TRACE)\\s+([a-zA-Z\\/.-]+)\\s+([a-zA-Z]+)\\/([0-9.]+)\r*$";
+    std::string pattern_resp = "HTTP\\/([0-9.]+)\\s+([0-9]+)\\s+(.*)\r*$";
+    std::string pattern_header = "^([a-zA-Z-]+):\\s+(.*)\r*$";
     
     std::regex regex_req(pattern_req);
     std::regex regex_resp(pattern_resp);
-    std::regex regex_header(pattern_header);
-    
-    bool body_trigger = false;
+    std::regex regex_header(pattern_header);    
 
     for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); it++) {
         std::cout << *it << std::endl;
-        if (std::regex_match(*it, match, regex_req) && !body_trigger) {
+        if (std::regex_match(*it, match, regex_req)) {
             this->msg_type = "request";
             this->entity_head = *it;
-        } else if (std::regex_match(*it, match, regex_resp ) && !body_trigger) {
+        } else if (std::regex_match(*it, match, regex_resp )) {
             this->msg_type = "response";
             this->entity_head = *it;
-        } else if (std::regex_match(*it, match, regex_header) && !body_trigger) {
+        } else if (std::regex_match(*it, match, regex_header)) {
             this->header_map[match[1]] = match[2];
         } else {
             if (*it != "") {
@@ -41,9 +39,6 @@ void BPHttpMessage::Parse(std::vector<std::string> lines) {
             }            
         }
     }
-    
-
-
 }
 
 std::string BPHttpMessage::HTTPDateFormatGet() {
