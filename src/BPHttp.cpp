4c5a923940c4a5bd9858a50d7f7e8bdbf0d6bd8d
logan <loganmitchell2011@gmail.com>
2023-03-30 22:31:54 -0400

    added Static SendRequest function to BPHttp

--- src/BPHttp.cpp
+++ src/BPHttp.cpp
@@ -66,6 +66,42 @@ std::string BPHttpMessage::ParseRequestResource(std::string ent_head) {
     return "";
 }
 
+bool BPHttpMessage::SendRequest(std::string host_ip, int port, std::string msg, std::string* out_data, std::string* err_out_data) {
+    char buffer[(1024*1024*5)] = {0};
+    sockaddr_in serverAddress;
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);    
+    if (clientSocket < 0) {
+        *err_out_data = "Failed to create client socket";
+        return false;
+    }
+    struct hostent* host = gethostbyname(host_ip.c_str());    
+    if (host != NULL) {
+        struct in_addr* addr = (struct in_addr*)host->h_addr;    
+        serverAddress.sin_addr.s_addr = addr->s_addr;
+    } else {
+        serverAddress.sin_addr.s_addr = inet_addr(host_ip.c_str());
+    }
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    
+    if (connect(clientSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0) {
+        *err_out_data = "Error connecting to server";
+        return false;
+    }
+    send(clientSocket, msg.c_str(), strlen(msg.c_str()), 0);
+    ssize_t valread = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (valread == -1) {
+        *err_out_data = "Error reading from socket";
+        return false;
+    } else if (valread == 0) {
+        *err_out_data = "Server shut down connection.";
+        return false;
+    }
+    *out_data = std::string(buffer);
+    close(clientSocket);
+    return true;
+}
+
 std::string BPHttpMessage::HTTPDateFormatGet() {
     return "";
 }
