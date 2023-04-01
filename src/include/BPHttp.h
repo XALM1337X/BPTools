689bb96836fa8b039e3768d6c3f78ddf1afeeb56
logan <loganmitchell2011@gmail.com>
2023-04-01 18:28:36 -0400

    formatting and added GenerateDate

--- src/include/BPHttp.h
+++ src/include/BPHttp.h
@@ -10,25 +10,19 @@ class BPHttpMessage {
         BPHttpMessage();
         ~BPHttpMessage();
         bool Parse(std::vector<std::string>);
-        //TODO: Add method to make this system call for the date:  date -u +"%a, %d %b %Y %H:%M:%S GMT"
         std::string HTTPDateFormatGet();
         std::string ParseRequestResource(std::string);
         static bool SendRequest(std::string, int, std::string, std::string*, std::string*);
-
         std::string BuildMessageString();
         void EntityHeadSet(std::string, std::string, std::string);
         void HeaderSet(std::string, std::string);
         void BodySet(std::string);
-        
-        void ClearBody();
-    
+        static void GenerateDate(std::string*, std::string*);        
+        void ClearBody();    
     public:
-        //HTTP Members
         std::string entity_head;
         std::unordered_map<std::string, std::string> header_map;  
         std::string body;
-
-        //Regex members   
         std::string pattern_req;
         std::string pattern_resp;
         std::string pattern_header;
@@ -36,8 +30,6 @@ class BPHttpMessage {
         std::regex* regex_resp;
         std::regex* regex_header;     
 
-        //Internals
-        std::string msg_type;
 };
 
 #endif
