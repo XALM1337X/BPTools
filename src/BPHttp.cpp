6468ca2ed12316ce4ec33e73d1848cf2ae90f3b0
logan <loganmitchell2011@gmail.com>
2023-04-01 18:27:29 -0400

    alterations to sendrequest

--- src/BPHttp.cpp
+++ src/BPHttp.cpp
@@ -66,12 +66,12 @@ std::string BPHttpMessage::ParseRequestResource(std::string ent_head) {
     return "";
 }
 
-bool BPHttpMessage::SendRequest(std::string host_ip, int port, std::string msg, std::string* out_data, std::string* err_out_data) {
-    char buffer[(1024*1024*5)] = {0};
+bool BPHttpMessage::SendRequest(std::string host_ip, int port, std::string msg, std::string* out_data, std::string* err_out) {
+    
     sockaddr_in serverAddress;
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);    
     if (clientSocket < 0) {
-        *err_out_data = "Failed to create client socket";
+        *err_out = "Failed to create client socket";
         return false;
     }
     struct hostent* host = gethostbyname(host_ip.c_str());    
@@ -85,21 +85,47 @@ bool BPHttpMessage::SendRequest(std::string host_ip, int port, std::string msg, 
     serverAddress.sin_port = htons(port);
     
     if (connect(clientSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0) {
-        *err_out_data = "Error connecting to server";
+        *err_out = "Error connecting to server";
         return false;
     }
+    // set the socket to blocking mode
+    int flags = fcntl(clientSocket, F_GETFL, 0);
+    fcntl(clientSocket, F_SETFL, flags & ~O_NONBLOCK);    
     send(clientSocket, msg.c_str(), strlen(msg.c_str()), 0);
-    ssize_t valread = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (valread == -1) {
-        *err_out_data = "Error reading from socket";
-        return false;
-    } else if (valread == 0) {
-        *err_out_data = "Server shut down connection.";
+    if (shutdown(clientSocket, SHUT_WR) == -1) {
+    // error handling code for shutdown function call
+        *err_out = "Error shutting down socket";
         return false;
     }
-    *out_data = std::string(buffer);
+    fd_set read_fds;
+    FD_ZERO(&read_fds);
+    FD_SET(clientSocket, &read_fds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+
+    int numReady = select(clientSocket + 1, &read_fds, NULL, NULL, &timeout);
+    if (numReady != -1 && numReady != 0) {
+        char* buffer = (char*)malloc(1024*1024*5);
+        ssize_t valread = 0;
+        size_t buffer_size = 1024*1024*5;
+        while ((valread = recv(clientSocket, buffer, buffer_size, 0)) > 0) {
+            buffer_size += valread;
+            buffer = (char*)realloc(buffer, buffer_size);
+            if (buffer == NULL) {
+                *err_out = "Failed to allocate memory for response buffer";
+        return false;
+    }
+            *out_data += std::string(buffer);
+            std::cout << buffer << std::endl;
+        }
+    }
+
     close(clientSocket);
-    return true;
+
+    //return true;
+    return false;
 }
 
 std::string BPHttpMessage::BuildMessageString() {
